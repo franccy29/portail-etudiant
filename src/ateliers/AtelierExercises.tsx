686acfb825ee2208be2decd326d661f0ce7cd8fd
François Martineau <87383252+franccy29@users.noreply.github.com>
2022-01-31 20:06:15 -0500

    logique de la vrai et false reponse est mis

--- src/ateliers/AtelierExercises.tsx
+++ src/ateliers/AtelierExercises.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { AtelierExercisesStyled } from "./styles/AtelierExercises.styles.js";
 
@@ -6,10 +6,43 @@ type Props = {
   atelierId: number;
 };
 
-const AtelierExercises: React.FC<Props> = ({ atelierId }) => {
+const data: { id: number; img: string; reponse: string; question: string } = {
+  id: 2,
+  reponse: "reponse",
+  img: "lien d'image",
+  question: "la question est ici",
+};
+
+const AtelierExercises: React.FC<Props> = () => {
+  const [answer, setAnswer] = useState<string>("");
+  const [tromper, setTromper] = useState<string>("");
+
+  const handleChange = (e: {
+    target: { value: React.SetStateAction<string> };
+  }): void => {
+    setAnswer(e.target.value);
+  };
+
+  const onSubmit = (): void => {
+    if (answer === data.reponse) {
+      setTromper("réussis");
+    } else {
+      setTromper("oui");
+    }
+  };
+
   return (
     <AtelierExercisesStyled>
-      <p>ca cest lexercise avec le id: {atelierId}</p>
+      <h2>{data.question}</h2>
+      <p>ici une image avec ce lien la: {data.img}</p>
+      <p>ca cest lexercise avec le id: {data.id}</p>
+      {tromper === "oui" ? (
+        <p>Ce n'est pas la reponse, try again!</p>
+      ) : tromper === "réussis" ? (
+        <p>bravo! je sais pas encore quoi faire quand cest réussis</p>
+      ) : null}
+      <input onChange={handleChange} />
+      <button onClick={onSubmit}>vérifie!</button>
     </AtelierExercisesStyled>
   );
 };
