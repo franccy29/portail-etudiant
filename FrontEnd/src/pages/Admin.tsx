a53a90fc5230fefac36a2d6308ed8d18d9f45a39
François Martineau <87383252+franccy29@users.noreply.github.com>
2022-02-04 01:04:41 -0500

    axios

--- FrontEnd/src/pages/Admin.tsx
+++ FrontEnd/src/pages/Admin.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import axios from "axios";
 
 import { AdminStyled } from "./styles/Admin.styles.js";
 
 type Props = {};
 
 const Admin: React.FC<Props> = (props) => {
+  axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+    const persons = res.data;
+    console.log(persons);
+  });
   return (
     <AdminStyled>
       <p>ici cest la partie admin, il faut le securisé</p>
