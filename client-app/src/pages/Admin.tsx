85a091ed4417ec82515ee7e607da1802f97964f8
François Martineau <87383252+franccy29@users.noreply.github.com>
2022-02-04 00:46:38 -0500

    commit random

--- client-app/src/pages/Admin.tsx
+++ client-app/src/pages/Admin.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React from "react";
 
 import { AdminStyled } from "./styles/Admin.styles.js";
@@ -5,6 +6,11 @@ import { AdminStyled } from "./styles/Admin.styles.js";
 type Props = {};
 
 const Admin: React.FC<Props> = (props) => {
+  axios.get("https://localhost:7204/test").then((res) => {
+    const persons = res.data;
+    console.log(persons);
+  });
+
   return (
     <AdminStyled>
       <p>ici cest la partie admin, il faut le securisé</p>
