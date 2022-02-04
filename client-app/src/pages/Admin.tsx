a3bce0a36cb9fec9f43c57163c11dc36930c7373
François Martineau <87383252+franccy29@users.noreply.github.com>
2022-02-04 00:46:45 -0500

    Revert "commit random"
    This reverts commit 85a091ed4417ec82515ee7e607da1802f97964f8.

--- client-app/src/pages/Admin.tsx
+++ client-app/src/pages/Admin.tsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import React from "react";
 
 import { AdminStyled } from "./styles/Admin.styles.js";
@@ -6,11 +5,6 @@ import { AdminStyled } from "./styles/Admin.styles.js";
 type Props = {};
 
 const Admin: React.FC<Props> = (props) => {
-  axios.get("https://localhost:7204/test").then((res) => {
-    const persons = res.data;
-    console.log(persons);
-  });
-
   return (
     <AdminStyled>
       <p>ici cest la partie admin, il faut le securisé</p>
